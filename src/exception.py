0ec2ee2e34ea16b5df9b6fe193ff3e4ea2c3228f
Nikhil Murade <muradenikhil@gmail.com>
2026-01-27 12:34:48 +0530

    meow

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys 
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
 
@@ -22,10 +22,10 @@ class CustomException(Exception):
         return self.error_message
 
 
-# if __name__ == "__main__":
-#     try:
-#         1/0
-#     except Exception as e:
-#         logging.info("Divide by zero error")
-#         raise CustomException(e, sys)
+if __name__ == "__main__":
+    try:
+        1/0
+    except Exception as e:
+        logging.info("Divide by zero error")
+        raise CustomException(e, sys)
     
